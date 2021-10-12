815102f59ed3908d1fe45ded2297434e7dcebdbe
Dileka Gunawardana <sdg2@rice.edu>
2021-10-12 12:54:39 -0500

    Updates on Bayes Factor

--- R/bayes_factor.R
+++ R/bayes_factor.R
@@ -7,7 +7,9 @@ predict = FALSE
 range_false_predicts = 10
 standardize = FALSE
 covariate_probabilities = NULL
-
+desired_sparsity = NULL
+desired_prior_effect = NULL
+train_percent = NULL
 
 bayes_factor <- function(data, response, desired_sparsity, 
                          predict, range_false_predicts, standardize,
@@ -27,29 +29,85 @@ bayes_factor <- function(data, response, desired_sparsity,
     }
   }
   
+  if (train == TRUE) {
+    if (is.null(train_percent) == TRUE){return("Please input a percent to be set aside for training")}
+    train_data_length <- round(train_percent*nrow(data), digits = 0)
+    train_data_ind <- sample(1:nrow(data), size = train_data_length, replace = FALSE)
+    train <- data[train_data_ind]
+    test <- data[-train_data_ind]
+    data <- train
+    y <- y[train_data_ind]
+    test_y <- y[-train_data_ind]
+  }
+  
   ### First layer of Decision Tree (BIC Bayes Factor)
   if (no_prior_information == TRUE){
     BIC_function(data)
     break
   }
-  
+
   ### Second Layer of Decision Tree ()
   if (is.null(covariate_probabilities) == FALSE){
-    if (ncol(data) <= 25){
-      bf_obj <- Bvs(formula = response ~ ., 
-          data=data.frame(data,response), 
-          prior.betas = "Robust",
-          prior.models = "User",
-          priorprobs = covariate_probabilities)
-    }
-    else{
-      bf_obj <- Bvs(formula = response ~ ., 
-          data=data.frame(data,response), 
-          prior.betas = "Robust",
-          prior.models = "User",
-          priorprobs = covariate_probabilities)
+    for (prior in c("Robust", "gZellner", "Liangetal", "ZellnerSiow", "FLS")){
+      # Small p
+      if (ncol(data) <= 25){
+        paste0("bf_obj", prior) <- Bvs(formula = response ~ ., 
+                      data=data.frame(data,response), 
+                      prior.betas = prior,
+                      prior.models = "User",
+                      priorprobs = covariate_probabilities)
+        return(paste(prior) + paste0("bf_obj", prior))
+        coef <- BMAcoeff(paste0("bf_obj", prior))
+        for (col in colnames(data)){
+          return(histBMA(coef, covariate = col))
+        }
+        if (train == TRUE){
+          return(predict.Bvs(paste0("bf_obj", prior), test))
+        }
+      }
+      # Large p
+      else{
+        paste0("bf_obj", prior) <- GibbsBvs(formula = response ~ ., 
+                           data=data.frame(data,response), 
+                           prior.betas = prior,
+                           prior.models = "User",
+                           priorprobs = covariate_probabilities)
+        return(final_output(paste0("bf_obj", prior)), predict)
+      }
     }
   }
+  
+  ### Third Layer of Decision Tree (BayesFactor)
+  if ((desired_sparisty <= 0.4 && desired_prior_effect < 0.5) || 
+      (desired_sparsity <= 0.4 && is.null(desired_prior_effect) == TRUE) ||
+      (is.null(desired_sparsity) == TRUE && desired_prior_effect < 0.5)){
+    return(regressionBF(formula = response~., 
+                        data= data.frame(data, reponse),
+                        rscaleCont = "medium",
+                        whichModels = "all"
+                        ))
+  }
+    if ((desired_sparisty >= 0.6 && desired_prior_effect > 0.5) || 
+        (desired_sparsity >= 0.6 && is.null(desired_prior_effect) == TRUE) ||
+        (is.null(desired_sparsity) == TRUE && desired_prior_effect > 0.5)){
+      return(regressionBF(formula = response~., 
+                          data= data.frame(data, reponse),
+                          rscaleCont = "medium",
+                          whichModels = "all"
+      ))
+    }
+      else {
+        return(regressionBF(formula = response~., 
+                            data= data.frame(data, response),
+                            rscaleCont = "wide",
+                            whichModels = "all"
+        ))
+        
+        # can i return two things here?
+
+  }
+  
+  
 }
 
 BIC_function <- function(data){
@@ -59,3 +117,5 @@ BIC_function <- function(data){
   BF_BIC  # Show it
   # return BF for all possible models
 }
+
+
--- R/test_file.R
+++ R/test_file.R
@@ -1,1 +1,15 @@
 #test_file
+
+n <- 20
+column_names <- list("one", "two", "three")
+type <- list("numerical", "numerical", "distributional")
+variables <- list(c(10:100), c(1, 2, 3), NA)
+weights <- list(rep(1/91, 91), c(0.3,0.3,0.4), NA)
+replace <- list(TRUE, TRUE, TRUE)
+distribution_type <- list(NA, NA, "rpois")
+distribution_inf <- list(NA, NA, c(4))
+
+df <- simulate(n, column_names, type, variables, weights, replace, distribution_type, distribution_inf)
+
+y <- 20*df$one + 10*df$three
+
