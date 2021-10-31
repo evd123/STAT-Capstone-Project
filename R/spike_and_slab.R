b3713e48587b00d405fdfbe7f8cb3970edf27f40
evd123 <46075902+evd123@users.noreply.github.com>
2021-10-31 14:42:14 -0500

    Add files via upload

--- R/spike_and_slab.R
+++ R/spike_and_slab.R
@@ -1,35 +1,102 @@
 
 ## SPIKE AND SLAB ##
 
-spike_and_slab_helper <- function(expected_sparsity, beta_variances, zellner_g) {
-  if (!is.na(expected_sparsity)) {
-    es <- 1
-    info <- expected_sparsity
-  } else {
-    if (!is.na(beta_variances)) {
-      es <- 2
-      info <- beta_variances
-    } else if (!is.na(zellner_g)){
-      es <- 3
-      info <- zellner_g
-    } else {
-      print ("An expected sparsity level, beta variances, or Zellner-G value is expected. If you do not have these, please try with prior_knowledge set to FALSE")
-    }
-  }
-  return (c(es, info))
-}
+# spike_and_slab_helper <- function(expected_sparsity, beta_variances, zellner_g) {
+#   if (!is.na(expected_sparsity)) {
+#     es <- 1
+#     info <- expected_sparsity
+#   } else {
+#     if (!is.na(beta_variances)) {
+#       es <- 2
+#       info <- beta_variances
+#     } else if (!is.na(zellner_g)){
+#       es <- 3
+#       info <- zellner_g
+#     } else {
+#       print ("An expected sparsity level, beta variances, or Zellner-G value is expected. If you do not have these, please try with prior_knowledge set to FALSE")
+#     }
+#   }
+#   return (c(es, info))
+# }
 
-spike_and_slab <- function(hyperparameters, p, ) {
-  if (hyperparameters == "zellner g") {
-    # calculate the prior
-    prior <- ConditionalZellnerPrior()
-    # fit the model
-    fit <- lm.spike
+spike_and_slab <- function(data, 
+                           predictors, 
+                           xdim, 
+                           response, 
+                           expected.r2, 
+                           prior.df, 
+                           expected_percent_nonzero, 
+                           zellner_g, 
+                           p, 
+                           prior.information.weight, 
+                           diagonal.shrinkage,
+                           max.flips,
+                           prior.inclusion.probabilities,
+                           mean.y,
+                           sdy,
+                           sigma.upper.limit) {
+  design <- model.matrix(predictors)
+  
+  if (!is.na(zellner_g)) {
+    if (is.na(object)) {
+      if (is.na(xdim) && !is.na(predictors)) {
+        xdim = dim(predictors)
+      } else if (is.na(xdim) && is.na(predictors)) {
+        print("Please enter the expected dimensions of the predictor matrix or the values of the predictors")
+        return()
+      }
+      # calculate the prior
+      prior <- ConditionalZellnerPrior(xdim, 
+                                       optional.coefficient.estimate = optional.coefficient.estimate,
+                                       expected.model.size = expected_percent_nonzero*p,
+                                       prior.information.weight = prior.information.weight,
+                                       diagonal.shrinkage = diagonal.shrinkage,
+                                       max.flips = max.flips,
+                                       prior.inclusion.probabilities = prior.inclusion.probabilities) # use if predictor matrix is unknown
+      prior
+      # fit the model
+      fit <- lm.spike(prior)
+      fit
+    } else {
+      # calculate the prior
+      prior <- SpikeSlabPrior(x = design,
+                              y = response,
+                              expected.r2 = expected.r2,
+                              prior.df = prior.df,
+                              expected.model.size = expected_percent_nonzero*p,
+                              prior.information.weight = prior.information.weight,
+                              diagonal.shrinkage = 0,
+                              optional.coefficient.estimate = coeff_estimate,
+                              max.flips = max.flips,
+                              mean.y = mean.y,
+                              sdy = sdy,
+                              prior.inclusion.probabilities = prior.inclusion.probabilities,
+                              sigma.upper.limit = sigma.upper.limit)
+      prior
+      # fit the model
+      fit <- lm.spike(prior)
+      fit
+    }
+    
   } else {
     # calculate the prior
-    prior <- SpikeSlabPrior()
+    prior <- SpikeSlabPrior(x = design,
+                            y = response,
+                            expected.r2 = expected.r2,
+                            prior.df = prior.df,
+                            expected.model.size = expected_percent_nonzero*p,
+                            prior.information.weight = prior_weight,
+                            diagonal.shrinkage = diagonal_shrinkage,
+                            optional.coefficient.estimate = coeff_estimate,
+                            max.flips = max_flips,
+                            mean.y = mean_y,
+                            sdy = sd_y,
+                            prior.inclusion.probabilities = prior.inclusion.probabilities,
+                            sigma.upper.limit = sigma.upper.limit)
+    prior
     # fit the model
     fit <- lm.spike
+    fit
   }
   
 }
